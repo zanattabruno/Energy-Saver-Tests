58e906b8d9ff02a9168fff490754bead71f530ca
zanattabruno <gkbsa2504@gmail.com>
2024-02-29 20:59:37 +0000

    Update container tags and add new E2 nodes

--- scripts/deploy-energy-enviroment.sh
+++ scripts/deploy-energy-enviroment.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#python3 deploy-e2term.py 
+ python3 deploy-e2term.py 
 
 helm upgrade --install e2node1 ../helm-charts/e2sim-helm \
     --set image.args.e2term=10.43.0.225 \
@@ -8,7 +8,7 @@ helm upgrade --install e2node1 ../helm-charts/e2sim-helm \
     --set image.args.mnc=11 \
     --set image.args.nodebid=1 \
     --set image.args.port=30001 \
-    -n ricplt
+    -n ricplt --wait
 
 helm upgrade --install e2node2 ../helm-charts/e2sim-helm \
     --set image.args.e2term=10.43.0.225 \
@@ -16,12 +16,28 @@ helm upgrade --install e2node2 ../helm-charts/e2sim-helm \
     --set image.args.mnc=11 \
     --set image.args.nodebid=2 \
     --set image.args.port=30001 \
-    -n ricplt
+    -n ricplt --wait
+
+helm upgrade --install e2node3 ../helm-charts/e2sim-helm \
+    --set image.args.e2term=10.43.0.225 \
+    --set image.args.mcc=724 \
+    --set image.args.mnc=11 \
+    --set image.args.nodebid=3 \
+    --set image.args.port=30001 \
+    -n ricplt --wait
+
+helm upgrade --install e2node4 ../helm-charts/e2sim-helm \
+    --set image.args.e2term=10.43.0.225 \
+    --set image.args.mcc=724 \
+    --set image.args.mnc=11 \
+    --set image.args.nodebid=4 \
+    --set image.args.port=30001 \
+    -n ricplt --wait
 
 helm upgrade --install xappmonitoring1 ../helm-charts/bouncer-xapp \
     --set containers[0].image.name="zanattabruno/bouncer-rc" \
     --set containers[0].image.registry="registry.hub.docker.com" \
-    --set containers[0].image.tag="sbrc-24" \
+    --set containers[0].image.tag="ue_api" \
     --set containers[0].name="bouncer-xapp" \
     --set containers[0].command[0]="b_xapp_main" \
     --set containers[0].args[0]="--mcc" \
@@ -30,12 +46,12 @@ helm upgrade --install xappmonitoring1 ../helm-charts/bouncer-xapp \
     --set containers[0].args[3]="11" \
     --set containers[0].args[4]="--nodebid" \
     --set containers[0].args[5]="1" \
-    -n ricxapp
+    -n ricxapp --wait
 
 helm upgrade --install xappmonitoring2 ../helm-charts/bouncer-xapp \
     --set containers[0].image.name="zanattabruno/bouncer-rc" \
     --set containers[0].image.registry="registry.hub.docker.com" \
-    --set containers[0].image.tag="sbrc-24" \
+    --set containers[0].image.tag="ue_api" \
     --set containers[0].name="bouncer-xapp" \
     --set containers[0].command[0]="b_xapp_main" \
     --set containers[0].args[0]="--mcc" \
@@ -44,4 +60,35 @@ helm upgrade --install xappmonitoring2 ../helm-charts/bouncer-xapp \
     --set containers[0].args[3]="11" \
     --set containers[0].args[4]="--nodebid" \
     --set containers[0].args[5]="2" \
-    -n ricxapp
+    -n ricxapp --wait
+
+helm upgrade --install xappmonitoring3 ../helm-charts/bouncer-xapp \
+    --set containers[0].image.name="zanattabruno/bouncer-rc" \
+    --set containers[0].image.registry="registry.hub.docker.com" \
+    --set containers[0].image.tag="ue_api" \
+    --set containers[0].name="bouncer-xapp" \
+    --set containers[0].command[0]="b_xapp_main" \
+    --set containers[0].args[0]="--mcc" \
+    --set containers[0].args[1]="724" \
+    --set containers[0].args[2]="--mnc" \
+    --set containers[0].args[3]="11" \
+    --set containers[0].args[4]="--nodebid" \
+    --set containers[0].args[5]="3" \
+    -n ricxapp --wait
+
+helm upgrade --install xappmonitoring4 ../helm-charts/bouncer-xapp \
+    --set containers[0].image.name="zanattabruno/bouncer-rc" \
+    --set containers[0].image.registry="registry.hub.docker.com" \
+    --set containers[0].image.tag="ue_api" \
+    --set containers[0].name="bouncer-xapp" \
+    --set containers[0].command[0]="b_xapp_main" \
+    --set containers[0].args[0]="--mcc" \
+    --set containers[0].args[1]="724" \
+    --set containers[0].args[2]="--mnc" \
+    --set containers[0].args[3]="11" \
+    --set containers[0].args[4]="--nodebid" \
+    --set containers[0].args[5]="4" \
+    -n ricxapp --wait
+
+sleep 10
+kubectl apply -f experiment.yaml -n ricplt
