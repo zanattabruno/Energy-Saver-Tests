1938c8223b2a5f395b099aa10eb76c23ba5a36e2
zanattabruno <gkbsa2504@gmail.com>
2024-05-03 03:01:01 +0000

    Update deployment scripts and configuration files for energy-saver-rapp and environment manager

--- scripts/deploy-energy-enviroment.sh
+++ scripts/deploy-energy-enviroment.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
- python3 deploy-e2term.py 
+#This command must be run inside scripts folder
+
+python3 deploy-e2term.py 
+
+bash policy_enode_ue/create_policy_type.bash
 
 helm upgrade --install e2node1 ../helm-charts/e2sim-helm \
     --set image.args.e2term=10.43.0.225 \
     --set image.args.mcc=724 \
-    --set image.args.mnc=11 \
+    --set image.args.mnc=011 \
     --set image.args.nodebid=1 \
     --set image.args.port=30001 \
     -n ricplt --wait
@@ -13,7 +17,7 @@ helm upgrade --install e2node1 ../helm-charts/e2sim-helm \
 helm upgrade --install e2node2 ../helm-charts/e2sim-helm \
     --set image.args.e2term=10.43.0.225 \
     --set image.args.mcc=724 \
-    --set image.args.mnc=11 \
+    --set image.args.mnc=011 \
     --set image.args.nodebid=2 \
     --set image.args.port=30001 \
     -n ricplt --wait
@@ -21,7 +25,7 @@ helm upgrade --install e2node2 ../helm-charts/e2sim-helm \
 helm upgrade --install e2node3 ../helm-charts/e2sim-helm \
     --set image.args.e2term=10.43.0.225 \
     --set image.args.mcc=724 \
-    --set image.args.mnc=11 \
+    --set image.args.mnc=011 \
     --set image.args.nodebid=3 \
     --set image.args.port=30001 \
     -n ricplt --wait
@@ -29,7 +33,7 @@ helm upgrade --install e2node3 ../helm-charts/e2sim-helm \
 helm upgrade --install e2node4 ../helm-charts/e2sim-helm \
     --set image.args.e2term=10.43.0.225 \
     --set image.args.mcc=724 \
-    --set image.args.mnc=11 \
+    --set image.args.mnc=011 \
     --set image.args.nodebid=4 \
     --set image.args.port=30001 \
     -n ricplt --wait
@@ -43,7 +47,7 @@ helm upgrade --install xappmonitoring1 ../helm-charts/bouncer-xapp \
     --set containers[0].args[0]="--mcc" \
     --set containers[0].args[1]="724" \
     --set containers[0].args[2]="--mnc" \
-    --set containers[0].args[3]="11" \
+    --set containers[0].args[3]="011" \
     --set containers[0].args[4]="--nodebid" \
     --set containers[0].args[5]="1" \
     -n ricxapp --wait
@@ -57,7 +61,7 @@ helm upgrade --install xappmonitoring2 ../helm-charts/bouncer-xapp \
     --set containers[0].args[0]="--mcc" \
     --set containers[0].args[1]="724" \
     --set containers[0].args[2]="--mnc" \
-    --set containers[0].args[3]="11" \
+    --set containers[0].args[3]="011" \
     --set containers[0].args[4]="--nodebid" \
     --set containers[0].args[5]="2" \
     -n ricxapp --wait
@@ -71,7 +75,7 @@ helm upgrade --install xappmonitoring3 ../helm-charts/bouncer-xapp \
     --set containers[0].args[0]="--mcc" \
     --set containers[0].args[1]="724" \
     --set containers[0].args[2]="--mnc" \
-    --set containers[0].args[3]="11" \
+    --set containers[0].args[3]="011" \
     --set containers[0].args[4]="--nodebid" \
     --set containers[0].args[5]="3" \
     -n ricxapp --wait
@@ -85,7 +89,7 @@ helm upgrade --install xappmonitoring4 ../helm-charts/bouncer-xapp \
     --set containers[0].args[0]="--mcc" \
     --set containers[0].args[1]="724" \
     --set containers[0].args[2]="--mnc" \
-    --set containers[0].args[3]="11" \
+    --set containers[0].args[3]="011" \
     --set containers[0].args[4]="--nodebid" \
     --set containers[0].args[5]="4" \
     -n ricxapp --wait
@@ -93,5 +97,11 @@ helm upgrade --install xappmonitoring4 ../helm-charts/bouncer-xapp \
 helm upgrade --install handover-xapp ../helm-charts/handover-xapp \
     -n ricxapp --wait
 
-sleep 10
-kubectl apply -f experiment.yaml -n ricplt
+bash policy_enode_ue/create_policy_type.bash
+
+kubectl apply -f envmanager -n ricplt
+
+sleep 30
+
+helm upgrade --install energy-saver-rapp ../helm-charts/energy-saver-rapp \
+    -n ricrapp --wait
